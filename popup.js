ad89e84eb046a4875e8fd5d00d06aa337f612141
josh <jwu96@students.cs.ubc.ca>
2023-11-19 16:53:23 -0800

    fetching metadata from pipeline call

--- popup.js
+++ popup.js
@@ -26,16 +26,27 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, async tabs => {
         ],
     };
 
-    fetch(url, {
+    const response = await fetch(url, {
         method: 'POST',
         headers: headers,
         body: JSON.stringify(data),
     })
+    let str = await response.json();
+    console.log(str);
+    const run_id = str.slice(37);
+
+    const url1 = 'https://api-v2.agenthub.dev/plrun?run_id=' + run_id;
+    console.log(url1);
+    const headers1 = {
+        'x-auth-key': 'D0MQIhCfnnR2fkDiqLT7OUcmjBp1',
+    };
+
+    fetch(url1, {
+        method: 'GET',
+        headers: headers1,
+    })
         .then((response) => response.json())
         .then((data) => console.log(data));
-
-    
-    
 
 
     // // get article data with API
@@ -68,7 +79,7 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, async tabs => {
     //         },
     //         body: new URLSearchParams({
     //             text: result0,
-    //             max_sentences: '3'
+    //             max_sentences: '2'
     //         })
     //     };
 
@@ -77,63 +88,7 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, async tabs => {
     //         const result = await response.text();
     //         console.log(result);
     //         console.log(result.length)
-    // get article data with API
-    const url0 = 'https://lexper.p.rapidapi.com/v1.1/extract?url=https%3A%2F%2F' + slicedUrl;
-
-    // check if url0 is correct
-    console.log(url0)
-
-    const options0 = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '368fc61910msh5229c8482c7e7ebp1d788fjsn5f2a20926e62',
-            'X-RapidAPI-Host': 'lexper.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response0 = await fetch(url0, options0);
-        const result0 = await response0.text();
-        console.log(result0);
-
-        // use TextGears API to generate summary
-        const url = 'https://textgears-textgears-v1.p.rapidapi.com/summarize';
-        const options = {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'X-RapidAPI-Key': '368fc61910msh5229c8482c7e7ebp1d788fjsn5f2a20926e62',
-                'X-RapidAPI-Host': 'textgears-textgears-v1.p.rapidapi.com'
-            },
-            body: new URLSearchParams({
-                text: result0,
-                max_sentences: '2'
-            })
-        };
-        
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            console.log(result);
-            console.log(result.length)
-
-    //         //slice to include summary only
-    //         let summaryPosition = result.indexOf("summary") + 8;
-    //         console.log(summaryPosition)
-    //         let slicedResult = currentUrl.slice(summaryPosition);
-    //         console.log(slicedResult)
-
-    //         // display summary on HTML
-    //         document.getElementById("summary").innerHTML = slicedResult;
-
-    //     } catch (error) {
-    //         console.error(error);
     //     }
-
-    // } catch (error) {
-    //     console.error(error);
-    // }
-
 
 });
 
